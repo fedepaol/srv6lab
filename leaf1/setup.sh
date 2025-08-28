78d80ef5ceb4baa7b02a45869c2c86c9bca4a4ad
Federico Paolinelli <fpaoline@redhat.com>
2025-08-28 21:43:01 +0200

    Still testing
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- leaf1/setup.sh
+++ leaf1/setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #
 
-sysctl -w net.vrf.strict_mode=1
-sysctl -w net.ipv4.conf.all.rp_filter=0
+sysctl -w net.ipv4.conf.all.forwarding=1
+sysctl -w net.ipv6.conf.all.forwarding=1
 sysctl -w net.ipv6.seg6_flowlabel=1
 sysctl -w net.ipv6.conf.all.seg6_enabled=1
+sysctl -w net.vrf.strict_mode=1
 
+ip link add sr0 type dummy
+ip link set sr0 up
 
 ip address add 10.0.0.1/32 dev lo
 ip addr add  fd00:0:1::1/128 dev lo
--- spine/setup.sh
+++ spine/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
 
-sysctl -w net.vrf.strict_mode=1
-sysctl -w net.ipv4.conf.all.rp_filter=0
+sysctl -w net.ipv4.conf.all.forwarding=1
+sysctl -w net.ipv6.conf.all.forwarding=1
 sysctl -w net.ipv6.seg6_flowlabel=1
 sysctl -w net.ipv6.conf.all.seg6_enabled=1
-
+sysctl -w net.vrf.strict_mode=1
 
 ip addr add 2001:db8:cafe:f00d::2/64 dev eth1
 ip addr add 2001:db8:cafe:f00e::2/64 dev eth2
